f5ec43f0e4d63c6c22ff014d1420f9a3cc0e4448
hebersonaguiar <hebersonaguiar@gmail.com>
2020-10-13 22:17:54 -0300

    alterando view

--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -3,10 +3,10 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Example Component Testing</div>
+                    <div class="panel-heading">Sacode Parlamento</div>
 
                     <div class="panel-body">
-                        I'm an example component!
+                        Sacode Parlamento
                     </div>
                 </div>
             </div>
