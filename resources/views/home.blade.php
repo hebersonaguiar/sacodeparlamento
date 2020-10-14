9254a72ce170f18662e8242c70492f0bb33f6955
hebersonaguiar <hebersonaguiar@gmail.com>
2020-10-13 22:54:03 -0300

    incluindo novo painel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<example-component></example-component>
-<!-- <div class="container">
+<div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+           <painel titulo="Sacode Parlamento"></painel>
         </div>
     </div>
-</div> -->
+</div>
 @endsection
