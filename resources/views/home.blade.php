783403004db1e488663b08b29d854ac2607179ee
hebersonaguiar <hebersonaguiar@gmail.com>
2020-10-14 22:13:28 -0300

    Criando caixa

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,14 +8,13 @@
                <!-- Redes Sociais -->
                <div class="row">
                    <div class="col-md-4">
-                       <caixa qtd="80" titulo="Artigos" url="#" cor="orange" icone="ion ion-pie-graph"></caixa>
+                        <caixa qtd="80" titulo="Artigos" url="#" cor="orange" icone="ion ion-pie-graph"></caixa>
                    </div>
                    <div class="col-md-4">
-
+                        <caixa qtd="180" titulo="Usuários" url="#" cor="blue" icone="ion ion-person-stalker"></caixa>
                    </div>
-
                    <div class="col-md-4">
-
+                        <caixa qtd="10" titulo="Autores" url="#" cor="green" icone="ion ion-person"></caixa>
                    </div>
                </div>
            </painel>
