d27d1be091d8705832d13c5029099af50c444428
hebersonaguiar <hebersonaguiar@gmail.com>
2020-10-14 22:31:35 -0300

    Criando caixa

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-           <painel titulo="Sacode Parlamento">
-               <!-- Redes Sociais -->
-               <div class="row">
-                   <div class="col-md-4">
-                        <caixa qtd="80" titulo="Artigos" url="#" cor="orange" icone="ion ion-pie-graph"></caixa>
-                   </div>
-                   <div class="col-md-4">
-                        <caixa qtd="180" titulo="Usuários" url="#" cor="blue" icone="ion ion-person-stalker"></caixa>
-                   </div>
-                   <div class="col-md-4">
-                        <caixa qtd="10" titulo="Autores" url="#" cor="green" icone="ion ion-person"></caixa>
-                   </div>
-               </div>
-           </painel>
+<pagina tamanho="7">
+    <painel titulo="Sacode Parlamento">
+        <!-- Redes Sociais -->
+        <div class="row">
+            <div class="col-md-4">
+                <caixa qtd="80" titulo="Artigos" url="#" cor="orange" icone="ion ion-pie-graph"></caixa>
+            </div>
+            <div class="col-md-4">
+                <caixa qtd="180" titulo="Usuários" url="#" cor="blue" icone="ion ion-person-stalker"></caixa>
+            </div>
+            <div class="col-md-4">
+                <caixa qtd="10" titulo="Autores" url="#" cor="green" icone="ion ion-person"></caixa>
+            </div>
         </div>
-    </div>
-</div>
+    </painel>
+</pagina>
 @endsection
